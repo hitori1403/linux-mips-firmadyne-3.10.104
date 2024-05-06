8e57b6187cb61d6aa708b13707f2656a1df5d3b3
hitori1403 <hitori1403@gmail.com>
2024-05-06 10:44:09 +0700

    apply patches from firmadyne kernel-v3.10

--- kernel/module.c
+++ kernel/module.c
@@ -828,7 +828,9 @@ SYSCALL_DEFINE2(delete_module, const char __user *, name_user,
 {
 	struct module *mod;
 	char name[MODULE_NAME_LEN];
-	int ret, forced = 0;
+	int ret = 0, forced = 0;
+
+	return ret;
 
 	if (!capable(CAP_SYS_MODULE) || modules_disabled)
 		return -EPERM;
@@ -3370,6 +3372,8 @@ SYSCALL_DEFINE3(init_module, void __user *, umod,
 	int err;
 	struct load_info info = { };
 
+	return 0;
+
 	err = may_init_module();
 	if (err)
 		return err;
@@ -3388,6 +3392,8 @@ SYSCALL_DEFINE3(finit_module, int, fd, const char __user *, uargs, int, flags)
 {
 	int err;
 	struct load_info info = { };
+
+	return 0;
 
 	err = may_init_module();
 	if (err)
